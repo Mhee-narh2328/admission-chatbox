f2fc27e24c59399044b33f3d2e7b9d25c3082117
Mhee-narh2328 <100017861+Mhee-narh2328@users.noreply.github.com>
2023-10-10 13:06:22 +0100

    worked on chat history

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from chat import get_response
+from datetime import datetime
 import model
 import os
 from db import db
@@ -21,6 +22,13 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(250), nullable = False)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
+
+class ChatMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(500))
+    time = db.Column(db.DateTime, default = datetime.now)
+    user = db.relationship('User', backref=db.backref('messages', lazy=True))
 
 app.config['SECRET_KEY'] = "vdbjcdgyhjsdghsc"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///unichat.db'
@@ -42,10 +50,19 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-@app.route("/index", methods = ["GET"])
+@app.route("/index", methods = ["GET", "POST"])
 @login_required
 def index_get():
-    return render_template("base.html")
+    user = User.query.get(current_user.id)
+    message = request.form.get('message')
+    if message:
+        new_message = ChatMessage(user_id=current_user.id, message = message)
+        db.session.add(new_message)
+        db.session.commit()
+    user_id = current_user.id
+    messages = ChatMessage.query.filter_by(user_id=user_id).all()
+    # print(messages)
+    return render_template("base.html", user = user, messages = messages)
 
 
 @app.route("/predict", methods = ["GET", "POST"])
@@ -90,17 +107,19 @@ def register():
 @app.route("/login",methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
+        name =  request.form.get("full_name")
         email = request.form.get("email")
         password = request.form.get("password")
 
+        name = User.query.filter_by(name = name).first()
         user = User.query.filter_by(email = email).first()
-        if user:
+        if user and name:
             if check_password_hash(user.password, password):
                 flash("Logged in!", category = "success")
                 login_user(user, remember = True)
                 return redirect(url_for('index_get'))
             else:
-                flash("Password is incorrect",category = "error")
+                flash("Password is incorrect",category = "error")  
         else:
             flash("Email doesn't exist",category = "error")
     return render_template("login.html")
